394d3cded9d9c26e5c6dc07ce3f108ef8317f042
noceive <nocivescience@gmail.com>
2022-08-10 00:22:52 -0400

    terminado

--- interpolate.py
+++ interpolate.py
@@ -2,9 +2,11 @@ from manim import *
 class InterpolateScene2(Scene):
     def construct(self):
         amps=[t for t in np.random.uniform(-3,3,size=14)]
-        func_1=self.get_many_func(amps)
-        self.play(Create(func_1))
-        self.wait()
+        funcs=self.get_many_func(amps)
+        flow_lines=self.get_flow_lines(funcs)
+        self.play(Create(funcs))
+        self.add(flow_lines)
+        self.wait(18)
     def get_many_func(self,amps_var):
         return VGroup(*[
             FunctionGraph(
@@ -24,7 +26,12 @@ class InterpolateScene2(Scene):
                 max(interpolate(-window,1,alpha),0),
                 min(interpolate(0,1+window,alpha),1)
             )
-            result=VGroup()
-            for template in func_group:
-                circle=template.copy()
-                circle.set_Stroke(color=interpolate_color(BLUE_A,BLUE_E),np.random.random())
+        result=VGroup()
+        for template in func_group:
+            circle=template.copy()
+            circle.set_stroke(color=interpolate_color(BLUE_A,BLUE_E,np.random.random()),width=3)
+            circle.template=template
+            circle.total_time=4*np.random.random()
+            circle.add_updater(update_circle)
+            result.add(circle)
+        return result
